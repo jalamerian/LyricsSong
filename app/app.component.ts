8c15fc965607b0b9f8ef0ce871af9ab3ebde0a4c
rhanbIT <aniel.jeanbaptiste@gmail.com>
2017-10-19 08:57:07 +0200

    Forgot update package.json

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core"; // décorateur
 
 @Component({
-  selector: "ns-app",
-  templateUrl: "app.component.html"
+  selector: "ns-app", // nom de notre élément html: <ns-app></ns-app>
+  templateUrl: "app.component.html" // chemin vers notre code html
+  //styleUrls: ["./app.component.css"] // les différents chemins vers nos
+  //feuilles de styles que l'on veut appliquer à ce component
 })
-export class AppComponent { }
+export class AppComponent {
+  public items: Array<any>;
+  public tabSelectedIndex: number;
+
+  constructor() {
+      this.tabSelectedIndex = 1;
+      this.items = new Array<any>();
+      for (let i = 0; i < 5; i++) {
+          this.items.push("item " + i);
+      }
+  }
+}
