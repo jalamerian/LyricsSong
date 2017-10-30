d3917757665c39718408b5958f11704b6309fa03
rhanbIT <aniel.jeanbaptiste@gmail.com>
2017-10-30 11:16:27 +0100

    For ngClass

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,19 @@
 import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NSModuleFactoryLoader } from "nativescript-angular/router";
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
+
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { NSModuleFactoryLoader } from "nativescript-angular/router";
+
+import {NativeScriptFormsModule } from 'nativescript-angular/forms';
+
+//services
+import { ArticleService } from './article/article.service';
+
+//components
 import { AppComponent } from "./app.component";
+
+
 
 @NgModule({
   bootstrap: [
@@ -17,7 +27,8 @@ import { AppComponent } from "./app.component";
     AppComponent
   ],
   providers: [
-    { provide: NgModuleFactoryLoader, useClass: NSModuleFactoryLoader }
+    { provide: NgModuleFactoryLoader, useClass: NSModuleFactoryLoader },
+    ArticleService
   ],
   schemas: [
     NO_ERRORS_SCHEMA
---
+++ app/article/article.model.ts
@@ -0,0 +1,8 @@
+export interface Article {
+  id: number,
+  title: string,
+  creationDate: number,
+  likes: number,
+  dislikes: number,
+  thumbnail: string
+}
